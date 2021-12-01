f2733cabb89ad2f1b4f8e6e24a4f6eefc1c3552b
Cédric Roy <cecric.roy@broadsign.com>
2021-12-01 10:02:14 -0500

    day 1

---
+++ advent_of_code2021/day_1.py
@@ -0,0 +1,38 @@
+def count_increases(data):
+    increases_count = []
+    for i in range(len(data) - 1):
+        increased = True if data[i] < data[i + 1] else False
+        increases_count.append(increased)
+    return sum(increases_count)
+
+
+def get_data_from_file(filename):
+    with open(filename) as f:
+        file_data = f.read()
+    return file_data
+
+
+def extract_depth_data(depth_data):
+    return [int(_) for _ in depth_data.split("\n")]
+
+
+def main(filename, count_function):
+    raw_depth_data = get_data_from_file(filename)
+    depth_data = extract_depth_data(raw_depth_data)
+    nb_of_increase = count_function(depth_data)
+    print(f"The number of increase is {nb_of_increase}")
+
+
+def get_windows_values(data):
+    max_items = len(data) - 1
+    windows = []
+    for i in range(len(data)):
+        if i + 3 > max_items + 1:
+            break
+        windows.append(sum([data[i], data[i + 1], data[i + 2]]))
+    return count_increases(windows)
+
+
+if __name__ in "__main__":
+    main("day_1.txt", count_increases)
+    main("day_1.txt", get_windows_values)
---
+++ tests/test_day1.py
@@ -0,0 +1,55 @@
+from unittest import mock
+
+from advent_of_code2021.day_1 import (
+    count_increases,
+    get_data_from_file,
+    main,
+    extract_depth_data,
+    get_windows_values,
+)
+
+
+def test_count_increases():
+    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    number = count_increases(data)
+    assert number == 7
+
+
+def test_get_data_from_file():
+    file_data = """1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc"""
+    mock_open = mock.mock_open(read_data=file_data)
+    with mock.patch("builtins.open", mock_open):
+        data = get_data_from_file("day_2.txt")
+        mock_open.assert_called_once_with("day_2.txt")
+
+
+def test_extract_depth_data():
+    data = [0, 1, 2, 3]
+    joined_raw_data = "\n".join([str(_) for _ in data])
+    results = extract_depth_data(joined_raw_data)
+    assert results == data
+
+
+@mock.patch("builtins.print")
+@mock.patch("advent_of_code2021.day_1.extract_depth_data")
+@mock.patch("advent_of_code2021.day_1.get_data_from_file")
+def test_main(mocked_get_data_from_file, mocked_extract_depth_data, mocked_print):
+    data = [0, 1, 2, 3]
+    joined_raw_data = "\n".join([str(_) for _ in data])
+    mocked_get_data_from_file.return_value = joined_raw_data
+    mocked_extract_depth_data.return_value = data
+    mocked_count_function = mock.Mock()
+    mocked_count_function.return_value = 7
+    main("filename.txt", mocked_count_function)
+    mocked_get_data_from_file.assert_called_once_with("filename.txt")
+    mocked_extract_depth_data.assert_called_once_with(joined_raw_data)
+    mocked_count_function.assert_called_once_with(data)
+    mocked_print.assert_called_once_with("The number of increase is 7")
+
+
+def test_get_windows():
+    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    nb_of_increases = get_windows_values(data)
+    assert nb_of_increases == 5
