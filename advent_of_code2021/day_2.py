f27a7c828e319315a364ea0dbd7d396daf5412b7
Cédric Roy <cecric.roy@broadsign.com>
2021-12-02 08:09:58 -0500

    day 2 part 1

---
+++ advent_of_code2021/day_2.py
@@ -0,0 +1,66 @@
+def execute_instructions_on_submarine(position, moves):
+    for move_command in moves:
+        position = execute_move(move_command, position)
+    return position
+
+
+def execute_move(move_command, position):
+    command, move = parse_move(move_command)
+    handler = get_command_handler(command)
+    position = handler(position, move)
+    return position
+
+
+def parse_move(move_command):
+    command, position = move_command.split(" ")
+    return command, int(position)
+
+
+def get_command_handler(command):
+    if command == "forward":
+        return forward_handler
+    if command == "up":
+        return up_handler
+    return down_handler
+
+
+def forward_handler(position, move):
+    position["h"] = position["h"] + move
+    return position
+
+
+def down_handler(position, move):
+    position["d"] = position["d"] + move
+    return position
+
+
+def up_handler(position, move):
+    position["d"] = position["d"] - move
+    return position
+
+
+def calculate_move_and_mutiply_results(moves, position):
+    destination = execute_instructions_on_submarine(position, moves)
+    result = destination["h"] * destination["d"]
+    print(f"The multiplication of H and D = {result}")
+
+
+def main(filename):
+    file_data = get_data_from_file(filename)
+    moves = extract_moves_from_file_data(file_data)
+    position = {"h": 0, "d": 0}
+    calculate_move_and_mutiply_results(moves, position)
+
+
+def extract_moves_from_file_data(file_data):
+    return file_data.split("\n")
+
+
+def get_data_from_file(filename):
+    with open(filename) as f:
+        file_data = f.read()
+    return file_data
+
+
+if __name__ == '__main__':
+    main('day_2.txt')
---
+++ tests/test_day2.py
@@ -0,0 +1,86 @@
+from unittest import mock
+
+from advent_of_code2021.day_2 import (
+    execute_instructions_on_submarine,
+    parse_move,
+    get_command_handler,
+    forward_handler,
+    down_handler,
+    up_handler,
+    calculate_move_and_mutiply_results,
+)
+
+
+def test_execute_instructions_on_submarine():
+    moves = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2",
+    ]
+    position = {"h": 0, "d": 0}
+    result = execute_instructions_on_submarine(position, moves)
+    assert result == {"h": 15, "d": 10}
+
+
+def test_parse_move():
+    command, pos = parse_move("forward 5")
+    assert command == "forward"
+    assert pos == 5
+
+
+def test_get_command_handler():
+    command = "forward"
+    command_handler = get_command_handler(command)
+    assert command_handler == forward_handler
+
+    command = "down"
+    command_handler = get_command_handler(command)
+    assert command_handler == down_handler
+
+    command = "up"
+    command_handler = get_command_handler(command)
+    assert command_handler == up_handler
+
+
+def test_forward_handler():
+    move = 5
+    position = {"h": 0, "d": 0}
+    new_position = forward_handler(position, move)
+    assert new_position == {"h": 5, "d": 0}
+
+
+def test_down_handler():
+    move = 5
+    position = {"h": 0, "d": 0}
+    new_position = down_handler(position, move)
+    assert new_position == {"h": 0, "d": 5}
+
+
+def test_up_handler():
+    move = 3
+    position = {"h": 0, "d": 5}
+    new_position = up_handler(position, move)
+    assert new_position == {"h": 0, "d": 2}
+
+
+@mock.patch("builtins.print")
+@mock.patch("advent_of_code2021.day_2.execute_instructions_on_submarine")
+def test_calculate_move_and_multiply_results(
+    mocked_execute_instructions_on_submarine, mocked_print
+):
+    moves = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2",
+    ]
+    position = {"h": 0, "d": 0}
+    mocked_execute_instructions_on_submarine.return_value = {"h": 15, "d": 10}
+    calculate_move_and_mutiply_results(moves, position)
+    mocked_execute_instructions_on_submarine.assert_called_once_with(position, moves)
+    mocked_print.assert_called_once_with("The multiplication of H and D = 150")
