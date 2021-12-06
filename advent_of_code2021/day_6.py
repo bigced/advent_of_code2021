be0a8824c7fd5ef636f9bc8caf246dfc45bd62ab
Cédric Roy <cecric.roy@broadsign.com>
2021-12-06 10:22:45 -0500

    day 6 part 2

--- advent_of_code2021/day_6.py
+++ advent_of_code2021/day_6.py
@@ -8,29 +8,51 @@ class Fish:
     def __str__(self):
         return self.internal_timer
 
+
 def extract_initial_fishes(file_data):
-    return [Fish(int(f)) for f in file_data.split(',')]
+    return [Fish(int(f)) for f in file_data.split(",")]
 
 
 def calculate_number_of_fish(file_data, days):
     fishes = extract_initial_fishes(file_data)
+    fishes_states = [0] * 9
+    initial_fishes(fishes_states, fishes)
+    fishes_states = update_state_for_days(days, fishes_states)
+    return sum(fishes_states)
 
-    for day in range(1, days + 1):
-        updated_fishes = []
-        for fish in fishes:
-            fish.internal_timer -= 1
-            if fish.internal_timer < 0:
-                updated_fishes.append(Fish(8))
-                fish.internal_timer = 6
-            updated_fishes.append(fish)
-        fishes = updated_fishes
-    return(len(fishes))
+
+def update_state_for_days(days, fishes_states):
+    for day in range(days):
+        fishes_states = update_day_state(fishes_states)
+    return fishes_states
+
+
+def update_day_state(fishes_states):
+    day_0 = fishes_states.pop(0)
+    reset_extisting_fishes(day_0, fishes_states)
+    add_new_fish(day_0, fishes_states)
+    return fishes_states
+
+
+def add_new_fish(day_0, fishes_states):
+    fishes_states.append(day_0)
+
+
+def reset_extisting_fishes(day_0, fishes_states):
+    fishes_states[6] += day_0
+
+
+def initial_fishes(fishes_states, fishes):
+    for fish in fishes:
+        fishes_states[fish.internal_timer] += 1
+
 
 def main(filename, days):
     file_data = get_data_from_file(filename)
     result = calculate_number_of_fish(file_data, days)
     print(f"Number of fish after {days}: {result}")
 
-if __name__ == '__main__':
-    main('day_6.txt', 80)
-    main('day_6.txt', 256)
+
+if __name__ == "__main__":
+    main("day_6.txt", 80)
+    main("day_6.txt", 256)
--- tests/test_day6.py
+++ tests/test_day6.py
@@ -4,8 +4,9 @@ from advent_of_code2021.day_6 import extract_initial_fishes, calculate_number_of
 def test_extract_initial_fish():
     file_data = "3,4,3,1,2"
     fishes = extract_initial_fishes(file_data)
-    for idx, initial_state in enumerate([3,4,3,1,2]):
+    for idx, initial_state in enumerate([3, 4, 3, 1, 2]):
         assert initial_state == fishes[idx].internal_timer
+
 
 def test_calculate_number_of_fish_after_1_day():
     file_data = "3,4,3,1,2"
@@ -20,14 +21,23 @@ def test_calculate_number_of_fish_after_2_day():
     result = calculate_number_of_fish(file_data, days)
     assert result == 6
 
+
+def test_calculate_number_of_fish_after_3_day():
+    file_data = "3,4,3,1,2"
+    days = 3
+    result = calculate_number_of_fish(file_data, days)
+    assert result == 7
+
+
 def test_calculate_number_of_fish_after_18_days():
     file_data = "3,4,3,1,2"
     days = 18
-    result  = calculate_number_of_fish(file_data, days)
+    result = calculate_number_of_fish(file_data, days)
     assert result == 26
+
 
 def test_calculate_number_of_fish_after_80_days():
     file_data = "3,4,3,1,2"
     days = 80
-    result  = calculate_number_of_fish(file_data, days)
+    result = calculate_number_of_fish(file_data, days)
     assert result == 5934
